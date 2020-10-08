6c1ca93d17a66d3fe20850b691f3cf7de42ccba1
Viktor Kalchenko <mfhillows@gmail.com>
2020-10-08 23:12:56 +0300

    add task 6 again

--- tasks/task-6.js
+++ tasks/task-6.js
@@ -4,15 +4,26 @@ function baseColor() {
   mainInput.classList.remove("invalid");
   mainInput.classList.remove("valid");
 }
+
 mainInput.addEventListener("blur", changeColor);
 function changeColor() {
-  if (mainInput.value.length < mainInput.dataset.length) {
-    mainInput.classList.add("invalid");
-  } else if (mainInput.value.length >= mainInput.dataset.length) {
-    mainInput.classList.remove("invalid");
+  if (mainInput.value.length == mainInput.dataset.length) {
     mainInput.classList.add("valid");
   } else {
-    mainInput.classList.remove("invalid");
     mainInput.classList.remove("valid");
+    mainInput.classList.add("invalid");
   }
 }
+
+// Variable 2
+// mainInput.addEventListener("blur", changeColor);
+// function changeColor() {
+//   if (mainInput.value.length > mainInput.dataset.length) {
+//     mainInput.classList.add("invalid");
+//   } else if (mainInput.value.length < mainInput.dataset.length) {
+//     mainInput.classList.add("invalid");
+//   } else {
+//     mainInput.classList.add("valid");
+//     mainInput.classList.remove("invalid");
+//   }
+// }
